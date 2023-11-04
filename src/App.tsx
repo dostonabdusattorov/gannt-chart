f43433a2ba1dafef4b9a8dc525aa1c13c72d265e
dostonabdusattorov <webdeveloperdoston@gmail.com>
2023-11-04 18:52:24 +0500

    set examples of task and initial filtering added

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,85 @@
-import {
-  Gantt,
-  Task,
-  EventOption,
-  StylingOption,
-  ViewMode,
-  DisplayOption,
-} from "gantt-task-react";
-import "gantt-task-react/dist/index.css";
+import { Gantt, Task, ViewMode } from "gantt-task-react";
 import { useState } from "react";
+import { initTasks, getStartEndDateForProject } from "./helpers";
+import { ViewSwitcher } from "./ViewSwitcher";
 
-let tasksInit: Task[] = [
-  {
-    start: new Date(2020, 1, 1),
-    end: new Date(2020, 1, 2),
-    name: "Idea",
-    id: "Task 0",
-    type: "task",
-    progress: 45,
-    isDisabled: true,
-    styles: { progressColor: "#ffbb54", progressSelectedColor: "#ff9e0d" },
-  },
-  {
-    start: new Date(2020, 1, 1),
-    end: new Date(2020, 1, 2),
-    name: "Idea",
-    id: "Task 1",
-    type: "task",
-    progress: 45,
-    isDisabled: true,
-    styles: { progressColor: "#ffbb54", progressSelectedColor: "#ff9e0d" },
-    dependencies: ["Task 0"],
-  },
-];
+export default function App() {
+  const [view, setView] = useState<ViewMode>(ViewMode.Day);
+  const [tasks, setTasks] = useState<Task[]>(initTasks());
+  const [isChecked, setIsChecked] = useState(true);
+  let columnWidth = 60;
+  if (view === ViewMode.Month) {
+    columnWidth = 300;
+  } else if (view === ViewMode.Week) {
+    columnWidth = 250;
+  }
 
-function App() {
-  const [tasks, setTasks] = useState(tasksInit);
+  const handleTaskChange = (task: Task) => {
+    console.log("On date change Id:" + task.id);
+    let newTasks = tasks.map((t) => (t.id === task.id ? task : t));
+    if (task.project) {
+      const [start, end] = getStartEndDateForProject(newTasks, task.project);
+      const project =
+        newTasks[newTasks.findIndex((t) => t.id === task.project)];
+      if (
+        project.start.getTime() !== start.getTime() ||
+        project.end.getTime() !== end.getTime()
+      ) {
+        const changedProject = { ...project, start, end };
+        newTasks = newTasks.map((t) =>
+          t.id === task.project ? changedProject : t
+        );
+      }
+    }
+    setTasks(newTasks);
+  };
+
+  const handleTaskDelete = (task: Task) => {
+    const conf = window.confirm("Are you sure about " + task.name + " ?");
+    if (conf) {
+      setTasks(tasks.filter((t) => t.id !== task.id));
+    }
+    return conf;
+  };
+
+  const handleProgressChange = async (task: Task) => {
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+    console.log("On progress change Id:" + task.id);
+  };
+
+  const handleDblClick = (task: Task) => {
+    alert("On Double Click event Id:" + task.id);
+  };
+
+  const handleSelect = (task: Task, isSelected: boolean) => {
+    console.log(task.name + " has " + (isSelected ? "selected" : "unselected"));
+  };
+
+  const handleExpanderClick = (task: Task) => {
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+    console.log("On expander click Id:" + task.id);
+  };
+
   return (
     <div>
+      <ViewSwitcher
+        onViewModeChange={(viewMode: ViewMode) => setView(viewMode)}
+        onViewListChange={setIsChecked}
+        isChecked={isChecked}
+      />
+      <h3>Gantt With Unlimited Height</h3>
       <Gantt
         tasks={tasks}
-        viewMode={ViewMode.Day}
-        onClick={(task: Task) => {
-          console.log("click", task);
-
-          setTasks((prev) => {
-            return prev.map((el: Task) => {
-              if (el.id === task.id) {
-                return { ...el, progress: el.progress + 1 };
-              }
-
-              return el;
-            });
-          });
-        }}
-        onSelect={(task: Task) => {
-          console.log("select", task);
-        }}
-        onProgressChange={(task: Task) => {
-          console.log("progress change", task);
-        }}
-        onExpanderClick={(task: Task) => {
-          console.log("expander click", task);
-        }}
+        viewMode={view}
+        onDateChange={handleTaskChange}
+        onDelete={handleTaskDelete}
+        onProgressChange={handleProgressChange}
+        onDoubleClick={handleDblClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={isChecked ? "155px" : ""}
+        columnWidth={columnWidth}
       />
     </div>
   );
 }
-
-export default App;
