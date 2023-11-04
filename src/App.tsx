aa536bd6812846818f2a13f6620b73d0129a61d6
dostonabdusattorov <webdeveloperdoston@gmail.com>
2023-11-04 13:25:21 +0500

    project initiated, sass added and configurated first look of react-gannt-task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Gantt,
+  Task,
+  EventOption,
+  StylingOption,
+  ViewMode,
+  DisplayOption,
+} from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
+import { useState } from "react";
+
+let tasksInit: Task[] = [
+  {
+    start: new Date(2020, 1, 1),
+    end: new Date(2020, 1, 2),
+    name: "Idea",
+    id: "Task 0",
+    type: "task",
+    progress: 45,
+    isDisabled: true,
+    styles: { progressColor: "#ffbb54", progressSelectedColor: "#ff9e0d" },
+  },
+  {
+    start: new Date(2020, 1, 1),
+    end: new Date(2020, 1, 2),
+    name: "Idea",
+    id: "Task 1",
+    type: "task",
+    progress: 45,
+    isDisabled: true,
+    styles: { progressColor: "#ffbb54", progressSelectedColor: "#ff9e0d" },
+    dependencies: ["Task 0"],
+  },
+];
 
 function App() {
+  const [tasks, setTasks] = useState(tasksInit);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Gantt
+        tasks={tasks}
+        viewMode={ViewMode.Day}
+        onClick={(task: Task) => {
+          console.log("click", task);
+
+          setTasks((prev) => {
+            return prev.map((el: Task) => {
+              if (el.id === task.id) {
+                return { ...el, progress: el.progress + 1 };
+              }
+
+              return el;
+            });
+          });
+        }}
+        onSelect={(task: Task) => {
+          console.log("select", task);
+        }}
+        onProgressChange={(task: Task) => {
+          console.log("progress change", task);
+        }}
+        onExpanderClick={(task: Task) => {
+          console.log("expander click", task);
+        }}
+      />
     </div>
   );
 }
